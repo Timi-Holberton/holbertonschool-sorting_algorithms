a7f972a17f67ea64388a6e5eeba1fead70206b1a
Timi-Holberton <timi.holberton@gmail.com>
2025-04-10 14:24:40 +0200

    ajout commentaire + correctif betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, echange;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)	/* i compte un passage jusqu'à ce que le*/
+					/* plus grand nombre soit à sa place */
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)	/* j avance dans le tableau sans*/
+	/*prendre en compte le dernier élément puisque j + 1 dans la condition*/
+/* il déduit le nombre de tour de la première boucle, size = 10, 9, 8...*/
 		{
-			if (array[j] > array[j + 1])
-			{
-				echange = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = echange;
+			if (array[j] > array[j + 1])	/* si l'élément de gauche */
+			{				/* est plus grand que celui de droite */
+				echange = array[j];	/* on garde l'élément de gauche */
+				array[j] = array[j + 1];	/* on met le plus petit à gauche */
+				array[j + 1] = echange;	/* on met le plus grand à droite */
 			}
+			print_array(array, size);	/* on affiche le tableau */
 		}
-
 	}
 }
